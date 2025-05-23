d489230e49b55a4e00ea07998286facd9cb80c82
txgao <txgao71@gmail.com>
2025-05-23 11:52:26 -0700

    update struct in data repository

--- internal/repository/memory/object_metadata_repository.go
+++ internal/repository/memory/object_metadata_repository.go
@@ -6,33 +6,34 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
+	"github.com/tendant/simple-content/internal/domain"
 	"github.com/tendant/simple-content/internal/repository"
 )
 
 // ObjectMetadataRepository is an in-memory implementation of the ObjectMetadataRepository interface
 type ObjectMetadataRepository struct {
 	mu       sync.RWMutex
-	metadata map[uuid.UUID]map[string]interface{}
+	metadata map[uuid.UUID]*domain.ObjectMetadata
 }
 
 // NewObjectMetadataRepository creates a new in-memory object metadata repository
 func NewObjectMetadataRepository() repository.ObjectMetadataRepository {
 	return &ObjectMetadataRepository{
-		metadata: make(map[uuid.UUID]map[string]interface{}),
+		metadata: make(map[uuid.UUID]*domain.ObjectMetadata),
 	}
 }
 
 // Set sets metadata for an object
-func (r *ObjectMetadataRepository) Set(ctx context.Context, objectID uuid.UUID, metadata map[string]interface{}) error {
+func (r *ObjectMetadataRepository) Set(ctx context.Context, metadata *domain.ObjectMetadata) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	r.metadata[objectID] = metadata
+	r.metadata[metadata.ObjectID] = metadata
 	return nil
 }
 
 // Get retrieves metadata for an object
-func (r *ObjectMetadataRepository) Get(ctx context.Context, objectID uuid.UUID) (map[string]interface{}, error) {
+func (r *ObjectMetadataRepository) Get(ctx context.Context, objectID uuid.UUID) (*domain.ObjectMetadata, error) {
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
