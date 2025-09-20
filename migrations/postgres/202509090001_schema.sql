78a604524bfaeb87559e17c901fd2b57ec741fd3
Lei Wang <tendant@gmail.com>
2025-09-20 10:21:26 -0700

    clean up migration

--- migrations/postgres/202509090001_schema.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
--- Schema creation is now managed outside of goose migrations.
--- See migrations/manual/000_create_schema.sql for an example helper snippet.
-
--- +goose Down
--- No-op. Schema removal is handled outside of goose migrations.
