9bb828e17489acac86d396eca5bfdd03e4344b81
Lei Wang <tendant@gmail.com>
2025-09-20 09:46:49 -0700

    add schema screation

---
+++ migrations/manual/000_create_schema.sql
@@ -0,0 +1,4 @@
+-- Helper snippet to provision the application schema prior to running goose migrations.
+-- Adjust the schema name as needed for your deployment.
+CREATE SCHEMA IF NOT EXISTS content;
+
